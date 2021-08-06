f031116a4725df5a5a43664d30d85bb519d74dbf
xaaavier <88211003+xaaavier@users.noreply.github.com>
2021-08-06 23:38:32 +0200

    Update mpi.sh

--- mpi.sh
+++ mpi.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ## Variables
 ### Logiciels à supprimer
-bloats="baobab cheese epiphany gnome-{boxes,software,calendar,characters,clocks,contacts,dictionary,font-viewer,logs,maps,photos,user-docs,weather} gucharmap sushi abrtd* yelp totem"
 ### Logiciels à ajouter
-addsoftwares="bluefish filezilla keepassxc gimp wget optimizer evolution libreoffice-langpack-fr lollypop qownnotes terminator tcpdump gnome-tweaks gnome-extensions-app vivaldi-stable wireshark gnome-shell-extension-user-theme gnome-shell-extension-gsconnect picard balena-etcher-electron sublime-text sublime-merge virt-manager vlc"
+addsoftwares="terminator powerline filezilla keepassxc gimp wget optimizer evolution libreoffice-langpack-fr lollypop qownnotes tcpdump gnome-tweaks gnome-extensions-app vivaldi-stable wireshark gnome-shell-extension-user-theme gnome-shell-extension-gsconnect picard balena-etcher-electron sublime-text virt-manager libvirt"
 
 if [[ $EUID -ne 0 ]]
 then
@@ -16,22 +15,18 @@ fi
 ### Optimisations
 #### Choix du miroir le plus rapide
 function optimisations() {
-isfm=$(grep -c fastestmirror /etc/dnf/dnf.conf)
+`isfm=$(grep -c fastestmirror /etc/dnf/dnf.conf)
 	if [[ "$isfm" -eq "0" ]]
 		then
-			echo "fastestmirror=1" >> /etc/dnf/dnf.conf
-	fi
+			`echo "fastestmirror=1" >> /etc/dnf/dnf.conf` 2>/dev/null
+	fi` 2>/dev/null
 #### Réduit la taille des mise à jour
-isdr=$(grep -c "deltarpm=true" /etc/dnf/dnf.conf)
+`isdr=$(grep -c deltarpm /etc/dnf/dnf.conf)
 	if [[ "$isfr" -eq "0" ]]
 		then
-			echo "deltarpm=true" >> /etc/dnf/dnf.conf
-	fi
+			`echo "deltarpm=true" >> /etc/dnf/dnf.conf` 2>/dev/null
+	fi` 2>/dev/null
 clear
-#### Installe tuned et passe le pc en mode performance
-dnf install tuned -y 2>/dev/null
-systemctl enable --now tuned 2>/dev/null
-tuned-adm profile latency-performance 2>/dev/null
 }
 
 ### Sécurité
@@ -52,23 +47,21 @@ echo -e '\n\e[0;36m'"Activer le serveur SSH? \e[0;m(\e[1;32mo\e[0;m/\e[1;32mN\e[
 	read ssh
 		if [ "$ssh" = 'o' ]
 			then
-				'systemctl enable --now sshd.service' 2>/dev/null
+				`systemctl enable --now sshd.service` 2>/dev/null
 				echo -e '\e[0;31m'Le serveur SSH est maintenant activé!'\e[0;m'
 			else
 				echo
 		fi
 ##### Client
-echo -e '\n\e[0;36m'"Générer des clefs de chiffrement SSH? \e[0;m(\e[1;32mo\e[0;m/\e[1;32mN\e[0;m)"
+echo -e '\n\e[0;36m'"Authoriser l'utilisation de clefs RSA? \e[0;m(\e[1;32mo\e[0;m/\e[1;32mN\e[0;m)"
 	read sshc
 		if [ "$sshc" = 'o' ]
 			then
-				ssh-keygen -a 100 -t ed25519 -C "*****@***.com" -N "" -f ~/.ssh/id_ed25519 2>/dev/null
-				ssh-keygen -b 4096 -t rsa -C "*****@***.com" -N "" -f ~/.ssh/id_rsa 2>/dev/null
-				echo -e '\e[0;31m'Des clefs SSH ont étés générées!'\e[0;m'
+				`update-crypto-policies --set LEGACY` 2>/dev/null
+				echo -e '\e[0;31m'Clefs RSA authorisée.'\e[0;m'
 			else
 				echo
 		fi
-update-crypto-policies --set LEGACY
 clear
 }
 
@@ -76,27 +69,27 @@ clear
 function repository() {
 #### Ajout Dépot
 ##### RPM Fusion Free
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y 2>/dev/null
+`dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y` 2>/dev/null
 echo -e '\e[0;31m'Le dépot RPM Fusion FREE est maintenant installé!'\e[0;m'
 ##### RPM Fusion nonFree
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y 2>/dev/null
+`dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y` 2>/dev/null
 echo -e '\e[0;31m'Le dépot RPM Fusion NON-FREE est maintenant installé!'\e[0;m'
 ##### Third Party Software
-dnf install fedora-workstation-repositories 2>/dev/null
+`dnf install fedora-workstation-repositories -y` 2>/dev/null
 echo -e '\e[0;31m'"Third party software activé!"
 ##### flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 2>/dev/null
+`flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo` 2>/dev/null
 echo -e '\e[0;31m'"Le dépot Flathub est installé.."'\e[0;m'
 ##### Vivaldi
-dnf install dnf-utils -y 2>/dev/null
-dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo 2>/dev/null
+`dnf install dnf-utils -y` 2>/dev/null
+`dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo` 2>/dev/null
 echo -e '\e[0;31m'"Le dépot de Vivaldi est installé.."'\e[0;m'
 ##### Balena Etcher
-curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.rpm.sh' | -E bash 2>/dev/null
+`curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.rpm.sh' | sudo -E bash` 2>/dev/null
+echo -e '\e[0;31m'"Le dépot de Balena Etcher est installé.."'\e[0;m'
 ##### Sublime
-rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg 2>/dev/null
-dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo 2>/dev/null
-
+`rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg && dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo` 2>/dev/null
+echo -e '\e[0;31m'"Le dépot Sublime est installé.."'\e[0;m'
 }
 
 ### Pilotes
@@ -107,7 +100,7 @@ echo -e '\n\e[0;36m'"Installer les pilotes Nvidia? \e[0;m(\e[1;32mo\e[0;m/\e[1;3
 read addnvidia
 if [ "$addnvidia" = 'o' ]
 	then 
-		dnf install xorg-x11-drv-nvidia akmod-nvidia xorg-x11-drv-nvidia-cuda -y 2>/dev/null
+		`dnf install xorg-x11-drv-nvidia akmod-nvidia xorg-x11-drv-nvidia-cuda -y` 2>/dev/null
 		echo -e '\e[0;31m'"Pilotes Nvidia installés!"'\e[0;m'
 	else
 		echo
@@ -117,7 +110,7 @@ echo -e '\n\e[0;36m'"Installer les pilotes Broadcom? \e[0;m(\e[1;32mo\e[0;m/\e[1
 read addbroadcom
 if [ "$addbroadcom" = 'o' ]
 	then 
-		dnf install akmod-wl -y 2>/dev/null
+		`dnf install akmod-wl -y` 2>/dev/null
 		echo -e '\e[0;31m'"Pilotes Broadcom installés!"'\e[0;m'
 	else
 		echo
@@ -125,22 +118,27 @@ fi
 }
 ### Logiciels préinstallés
 function bloatwares() {
-dnf autoremove $bloats -y  2>/dev/null
+`dnf autoremove baobab cheese epiphany gnome-{boxes,software,calendar,characters,clocks,contacts,dictionary,font-viewer,logs,maps,photos,user-docs,weather,tour,terminal} rhythmbox totem gedit gucharmap sushi abrtd* yelp gnome-shell-extension-{apps-menu,background-logo,launch-new-instance,window-list,places-menu} -y` 2>/dev/null
 echo -e '\e[0;31m'"Logiciels préinstallé supprimé!"'\e[0;m'
 }
 ### Mise à jour
 function update() {
 echo -e '\n\e[0;36m'"Mise à jour en cours, merci de patienter"
-dnf upgrade -y 2>/dev/null
+`dnf upgrade -y` 2>/dev/null
 }
 ### Ajout de logiciel
 function software_add() {
 #### Codecs
-dnf group upgrade --with-optional Multimedia -y 2>/dev/null
+`dnf group upgrade --with-optional Multimedia -y` 2>/dev/null
 echo -e '\e[0;31m'"Codecs supplémentaires installés!"'\e[0;m'
-dnf install $addsoftwares -y 2>/dev/null
+`dnf install $addsoftwares -y` 2>/dev/null
 #Commenter la ligne ci-dessous s'il wireshark n'est pas installé
-usermod -a -G wireshark `who | cut -f1 -d":"` 2>/dev/null
+`usermod -a -G wireshark `who | cut -f1 -d":"` 2>/dev/null
+#Commenter les lignes ci-dessous si virt-manager n'est pas installé
+`groupadd --system libvirt` 2>/dev/null
+`usermod -a -G libvirt `who | cut -f1 -d":"` 2>/dev/null
+`sed -i -e "s/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g" /etc/libvirt/libvirtd.conf` 2>/dev/null
+`sed -i -e "s/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g" /etc/libvirt/libvirtd.conf` 2>/dev/null
 echo -e '\e[0;31m'"Logiciels installés!"'\e[0;m'
 }
 ### Reboot
@@ -164,6 +162,7 @@ echo -ne "
 \e[0;32m6)\e[0;m Mise à jour
 \e[0;32m7)\e[0;m Ajout de logiciels
 \e[0;32m8)\e[0;m Reboot
+\e[0;32m9)\e[0;m One Shot
 \e[0;32m0)\e[0;m Quitter
 N'essaie pas! Fais-le ou ne le fais pas! Il n'y a pas d'essai: "
         read a
@@ -176,6 +175,7 @@ N'essaie pas! Fais-le ou ne le fais pas! Il n'y a pas d'essai: "
 		6) update ; menu ;;
 		7) software_add ; menu ;;
 		8) reboot ; menu ;;
+		9) optimisations && security && repository && drivers && bloatwares && update && software_add ; menu ;;
 		0) exit 0 ;;
 		*) echo -e "\e[0;31mLa première fois cest une erreur, ensuite cest de l'obstination.\e[0;m" $clear; menu;;
         esac
